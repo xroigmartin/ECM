8a6215f5897203f36a263c82f43235135a053f54
xroigmartin <xroigmartin@gmail.com>
2020-08-28 10:21:34 +0200

    Create project ECM

---
+++ ecm/src/test/java/xroigmartin/ecm/EcmApplicationTests.java
@@ -0,0 +1,13 @@
+package xroigmartin.ecm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
