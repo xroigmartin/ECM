a720c12bd442bc404784bf8d04374cd1fabbc806
xroigmartin <xroigmartin@gmail.com>
2020-09-14 21:24:09 +0200

    Modify LoggingRest for execute more cases

--- ecm/src/main/java/xroigmartin/ecm/api/rest/LoggingRest.java
+++ ecm/src/main/java/xroigmartin/ecm/api/rest/LoggingRest.java
@@ -11,14 +11,33 @@ public class LoggingRest {
 	private static final Logger logger = LogManager.getLogger(LoggingRest.class);
 	 
     @RequestMapping("/")
-    public String index() {
-    	for(int i=0; i < 1000; i++) {
-	        logger.trace("A TRACE a Message");
-	        logger.debug("A DEBUG Message");
-	        logger.info("An INFO Message");
-	        logger.warn("A WARN Message");
-	        logger.error("An ERROR Message");
-    	}
+    public String index() throws InterruptedException{
+    	int loop = 0;
+    	
+    	do {
+	    	for(int i=0; i < 1000; i++) {
+	    		if(i%2==0) {
+	    			logger.trace("A TRACE a Message");
+	    		}
+	    		if(i%3==0) {
+	    			logger.debug("A DEBUG Message");
+	    		}
+	    		if(i%5==0) {
+	    			logger.info("An INFO Message");
+	    		}
+	    		if(i%7==0) {
+	    			logger.warn("A WARN Message");
+	    		}
+	    		if(i%11==0) {
+	    			logger.error("An ERROR Message");
+	    		}
+	    		if(i%13==0) {
+	    			logger.fatal("An ERROR Message");
+	    		}
+	    	}
+	    	Thread.sleep(1000);
+	    	loop++;
+    	}while(loop < 10);
  
         return "Howdy! Check out the Logs to see the output...";
     }
