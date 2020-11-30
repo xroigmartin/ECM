56fc3371b97466d446b6b7e8d402bac86395e7bd
Xavier Roig <xroigmartin@gmail.com>
2020-11-30 22:17:58 +0100

    feat: Create scripts with schema and data of domain table

---
+++ sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ecm.domain(code_domain, specification) VALUES ('SEX', 'Sex of person');
+INSERT INTO ecm.domain(code_domain, specification) VALUES ('LANG', 'Languages of application');
+
+COMMIT;
