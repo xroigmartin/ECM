917fb9287cfcbb19e23b3518f22049363c25f574
Xavier Roig <xroigmartin@gmail.com>
2020-11-30 23:47:53 +0100

    feat: Class for implementation of Domain Service

---
+++ ecm/src/main/java/xroigmartin/ecm/service/domain/impl/DomainServiceImpl.java
@@ -0,0 +1,48 @@
+package xroigmartin.ecm.service.domain.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import xroigmartin.ecm.model.domain.Domain;
+import xroigmartin.ecm.repository.domain.DomainRepository;
+import xroigmartin.ecm.service.domain.DomainService;
+
+@Service
+public class DomainServiceImpl implements DomainService {
+
+	@Autowired
+	private DomainRepository domainRepository;
+	
+	@Override
+	public List<Domain> findAllDomains() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Domain findDomainById(Long domainId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Domain findDomainByCodeDomain(String codeDomain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Domain storeDomain(Domain domain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Domain changeEnable(Long domainId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
