b70cadb9d3b79232d8dfa1c414a87913d23dc78b
Xavier Roig <xroigmartin@gmail.com>
2021-01-09 11:43:46 +0100

    feat: Manage requests with Dtos

--- ecm/src/main/java/xroigmartin/ecm/api/controller/DomainApiController.java
+++ ecm/src/main/java/xroigmartin/ecm/api/controller/DomainApiController.java
@@ -1,6 +1,7 @@
 package xroigmartin.ecm.api.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import xroigmartin.ecm.api.controller.dto.CreateDomainDto;
+import xroigmartin.ecm.api.controller.dto.DomainDto;
+import xroigmartin.ecm.api.controller.dto.converter.DomainDtoConverter;
 import xroigmartin.ecm.model.domain.Domain;
 import xroigmartin.ecm.service.domain.DomainService;
 
@@ -22,40 +26,49 @@ public class DomainApiController {
 
 	@Autowired
 	private DomainService domainService;
+	
+	@Autowired
+	private DomainDtoConverter domainDtoConverter;
 
 	@GetMapping("/getAllDomains")
-	public ResponseEntity<List<Domain>> getAllDomains() {
+	public ResponseEntity<List<DomainDto>> getAllDomains() {
 		
 		List<Domain> listOfDomains = domainService.findAllDomains();
 		
 		if(listOfDomains.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
-		else {
-			return ResponseEntity.ok(listOfDomains);
+		else {		
+			List<DomainDto> dtoList = listOfDomains.stream()
+													.map(domainDtoConverter::converToDto)
+													.collect(Collectors.toList());
+			return ResponseEntity.ok(dtoList);
 		}
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Domain> getDomainById(@PathVariable Long id){
+	public ResponseEntity<DomainDto> getDomainById(@PathVariable Long id){
 		Domain domain = domainService.findDomainById(id);
 		
 		if(domain == null) {
 			return ResponseEntity.notFound().build();
 		}
 		else {
-			return ResponseEntity.ok(domain);
+			return ResponseEntity.ok(domainDtoConverter.converToDto(domain));			
 		}
 	}
 
 	@PostMapping("/new")
-	public ResponseEntity<Domain> insertDomain(@RequestBody Domain newDomain){
-		Domain domainSaved = domainService.addDomain(newDomain);
-		return ResponseEntity.status(HttpStatus.CREATED).body(domainSaved);
+	public ResponseEntity<DomainDto> insertDomain(@RequestBody CreateDomainDto newDomain){
+		Domain addDomain = new Domain(newDomain.getCodeDomain(), newDomain.getCodeDomainText());
+		Domain domainSaved = domainService.addDomain(addDomain);
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+							 .body(domainDtoConverter.converToDto(domainSaved));
 	}
 
 	@PutMapping("/{id}/edit")
-	public ResponseEntity<Domain> editDomain(@RequestBody Domain domainEdit, @PathVariable Long id){
+	public ResponseEntity<DomainDto> editDomain(@RequestBody Domain domainEdit, @PathVariable Long id){
 		Domain domainStore = domainService.findDomainById(id);
 		if (domainStore != null){
 			domainStore.setCodeDomain(domainEdit.getCodeDomain());
@@ -63,20 +76,24 @@ public class DomainApiController {
 			if (domainStore.isEnable() != domainEdit.isEnable()){
 				domainStore.changeEnable();
 			}
-			return ResponseEntity.ok(domainService.saveDomain(domainStore));
+			
+			Domain domainSaved = domainService.saveDomain(domainStore);
+			
+			return ResponseEntity.ok(domainDtoConverter.converToDto(domainSaved));
 		}
 		return ResponseEntity.notFound().build();
 	}
 
 	@PutMapping("/{id}/enable")
-	public ResponseEntity<Domain> enableDomain(@PathVariable Long id){
+	public ResponseEntity<DomainDto> enableDomain(@PathVariable Long id){
 		Domain domainStore = domainService.findDomainById(id);
 		if(domainStore != null) {
 			if(!domainStore.isEnable()) {
-				return ResponseEntity.ok(domainService.changeEnable(id));
+				Domain domainChanged = domainService.changeEnable(id);
+				return ResponseEntity.ok(domainDtoConverter.converToDto(domainChanged));
 			}
 			else {
-				return ResponseEntity.ok(domainStore);
+				return ResponseEntity.ok(domainDtoConverter.converToDto(domainStore));
 			}
 		}
 		else {
@@ -85,18 +102,20 @@ public class DomainApiController {
 	}
 
 	@PutMapping("/{id}/disable")
-	public ResponseEntity<Domain> disableDomain(@PathVariable Long id){
+	public ResponseEntity<DomainDto> disableDomain(@PathVariable Long id){
 		Domain domainStore = domainService.findDomainById(id);
 		if(domainStore != null) {
 			if(domainStore.isEnable()) {
-				return ResponseEntity.ok(domainService.changeEnable(id));
+				Domain domainChanged = domainService.changeEnable(id);
+				return ResponseEntity.ok(domainDtoConverter.converToDto(domainChanged));
 			}
 			else {
-				return ResponseEntity.ok(domainStore);
+				return ResponseEntity.ok(domainDtoConverter.converToDto(domainStore));
 			}
 		}
 		else {
 			return ResponseEntity.notFound().build();
 		}
 	}
+	
 }
