ff357d29a991a0b7c612d6435c08fa207c49c73d
Xavier Roig <xroigmartin@gmail.com>
2021-02-03 22:55:06 +0100

    feat: Create script with domain_value table

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS ecm.domain (
     PRIMARY KEY (domain_id),
     UNIQUE KEY (code_domain)
 );
+
+CREATE TABLE IF NOT EXISTS ecm.domain_value (
+    domain_value_id INT(10) NOT NULL AUTO_INCREMENT COMMENT 'Domain value identifier',
+    value VARCHAR(5) NOT NULL COMMENT 'Value of domain',
+    is_enable BOOLEAN NOT NULL DEFAULT 1 COMMENT 'Mark if value of domain is enable or not',
+    domain_id int(10) NOT NULL COMMENT 'Id of domain to which value belong',
+    PRIMARY KEY(domain_value_id),
+    UNIQUE(value, domain_id),
+    FOREIGN KEY (domain_id) REFERENCES ecm.domain(domain_id)
+);
