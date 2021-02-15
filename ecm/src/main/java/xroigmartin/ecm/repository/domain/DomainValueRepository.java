434f7dcfef0f38b684c06469daeee5afc5b2ae9d
Xavier Roig <xroigmartin@gmail.com>
2021-02-15 19:06:28 +0100

    refactor: Change method name in DomainValueRepository

--- ecm/src/main/java/xroigmartin/ecm/repository/domain/DomainValueRepository.java
+++ ecm/src/main/java/xroigmartin/ecm/repository/domain/DomainValueRepository.java
@@ -5,12 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import xroigmartin.ecm.model.domain.Domain;
 import xroigmartin.ecm.model.domain.DomainValue;
 
 public interface DomainValueRepository extends JpaRepository<DomainValue, Long>{
-
-	List<DomainValue> findAllDomainValueByDomain(Domain domain);
 	
-	Optional<DomainValue> findDomainValueByValueAndDomain(String value, Domain domain);
+	List<DomainValue> findByDomainId(Long domainId);
+	
+	Optional<DomainValue> findByValueAndDomainId(String value, Long domainId);
 }
