cc32abeb741487b168203eee513454ec571b58b5
Xavier Roig <xroigmartin@gmail.com>
2021-02-17 17:15:12 +0100

    feat: add new methods in DomainValue Repo

--- ecm/src/main/java/xroigmartin/ecm/repository/domain/DomainValueRepository.java
+++ ecm/src/main/java/xroigmartin/ecm/repository/domain/DomainValueRepository.java
@@ -3,6 +3,7 @@ package xroigmartin.ecm.repository.domain;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import xroigmartin.ecm.model.domain.DomainValue;
@@ -10,8 +11,22 @@ import xroigmartin.ecm.model.domain.DomainValue;
 public interface DomainValueRepository extends JpaRepository<DomainValue, Long>{
 	
 	List<DomainValue> findByDomainId(Long domainId);
+	List<DomainValue> findByDomainId(Long domainId, Pageable pageable);
+	List<DomainValue> findByDomainIdAndIsEnable(Long domainId);
+	List<DomainValue> findByDomainIdAndIsEnable(Long domainId, Pageable pageable);
 	
 	Optional<DomainValue> findByValueAndDomainId(String value, Long domainId);
-	
+	Optional<DomainValue> findByValueAndDomainId(String value, Long domainId, Pageable pageable);
+	Optional<DomainValue> findByValueAndDomainIdAndIsEnable(String value, Long domainId);
+	Optional<DomainValue> findByValueAndDomainIdAndIsEnable(String value, Long domainId, Pageable pageable);
+
 	List<DomainValue> findByValueContainingAndDomainId(String value, Long domainId);
+	List<DomainValue> findByValueContainingAndDomainId(String value, Long domainId, Pageable pageable);
+	List<DomainValue> findByValueContainingAndDomainIdAndIsEnable(String value, Long domainId);
+	List<DomainValue> findByValueContainingAndDomainIdAndIsEnable(String value, Long domainId, Pageable pageable);
+	
+	List<DomainValue> findByValueContaining(String value);
+	List<DomainValue> findByValueContaining(String value, Pageable pageable);
+	List<DomainValue> findByValueContainingIsEnable(String value);
+	List<DomainValue> findByValueContainingIsEnable(String value, Pageable pageable);
 }
