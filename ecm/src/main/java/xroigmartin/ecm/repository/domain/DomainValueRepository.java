ba2be729d90ffc33ce3c019b7eda694d71015f68
Xavier Roig <xroigmartin@gmail.com>
2021-02-19 09:19:51 +0100

    feat: Add new method for repository of Domain Value

--- ecm/src/main/java/xroigmartin/ecm/repository/domain/DomainValueRepository.java
+++ ecm/src/main/java/xroigmartin/ecm/repository/domain/DomainValueRepository.java
@@ -3,6 +3,7 @@ package xroigmartin.ecm.repository.domain;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,22 +12,26 @@ import xroigmartin.ecm.model.domain.DomainValue;
 public interface DomainValueRepository extends JpaRepository<DomainValue, Long>{
 	
 	List<DomainValue> findByDomainId(Long domainId);
-	List<DomainValue> findByDomainId(Long domainId, Pageable pageable);
-	List<DomainValue> findByDomainIdAndIsEnable(Long domainId);
-	List<DomainValue> findByDomainIdAndIsEnable(Long domainId, Pageable pageable);
+	Page<DomainValue> findByDomainId(Long domainId, Pageable pageable);
+	List<DomainValue> findByDomainIdAndEnableTrue(Long domainId);
+	Page<DomainValue> findByDomainIdAndEnableTrue(Long domainId, Pageable pageable);
 	
-	Optional<DomainValue> findByValueAndDomainId(String value, Long domainId);
-	Optional<DomainValue> findByValueAndDomainId(String value, Long domainId, Pageable pageable);
-	Optional<DomainValue> findByValueAndDomainIdAndIsEnable(String value, Long domainId);
-	Optional<DomainValue> findByValueAndDomainIdAndIsEnable(String value, Long domainId, Pageable pageable);
-
-	List<DomainValue> findByValueContainingAndDomainId(String value, Long domainId);
-	List<DomainValue> findByValueContainingAndDomainId(String value, Long domainId, Pageable pageable);
-	List<DomainValue> findByValueContainingAndDomainIdAndIsEnable(String value, Long domainId);
-	List<DomainValue> findByValueContainingAndDomainIdAndIsEnable(String value, Long domainId, Pageable pageable);
+	List<DomainValue> findByValue(String value);
+	Page<DomainValue> findByValue(String value, Pageable pageable);
+	List<DomainValue> findByValueAndEnableTrue(String value);
+	Page<DomainValue> findByValueAndEnableTrue(String value, Pageable pageable);
 	
 	List<DomainValue> findByValueContaining(String value);
-	List<DomainValue> findByValueContaining(String value, Pageable pageable);
-	List<DomainValue> findByValueContainingIsEnable(String value);
-	List<DomainValue> findByValueContainingIsEnable(String value, Pageable pageable);
+	Page<DomainValue> findByValueContaining(String value, Pageable pageable);
+	List<DomainValue> findByValueContainingAndEnableTrue(String value);
+	Page<DomainValue> findByValueContainingAndEnableTrue(String value, Pageable pageable);
+	
+	Optional<DomainValue> findByValueAndDomainId(String value, Long domainId);
+	Optional<DomainValue> findByValueAndDomainIdAndEnableTrue(String value, Long domainId);
+
+	List<DomainValue> findByValueContainingAndDomainId(String value, Long domainId);
+	Page<DomainValue> findByValueContainingAndDomainId(String value, Long domainId, Pageable pageable);
+	List<DomainValue> findByValueContainingAndDomainIdAndEnableTrue(String value, Long domainId);
+	Page<DomainValue> findByValueContainingAndDomainIdAndEnableTrue(String value, Long domainId, Pageable pageable);
+	
 }
