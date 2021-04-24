4185ee78ac776b0bc525a36b5e9a7910b5dcd2a9
Xavier Roig <xroigmartin@gmail.com>
2021-04-24 20:52:25 +0200

    fix: insert default domain value

--- sql/data.sql
+++ sql/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO ecm.domain(code_domain, code_domain_text) VALUES ('SEX', 'Sex of person');
-INSERT INTO ecm.domainValue(value, domain_id) values("H", (SELECT domain_id from ecm.domain where code_domain="SEX"));
-INSERT INTO ecm.domainValue(value, domain_id) values("M", (SELECT domain_id from ecm.domain where code_domain="SEX"));
+INSERT INTO ecm.domain_value(value, domain_id) values("H", (SELECT domain_id from ecm.domain where code_domain="SEX"));
+INSERT INTO ecm.domain_value(value, domain_id) values("M", (SELECT domain_id from ecm.domain where code_domain="SEX"));
 INSERT INTO ecm.domain(code_domain, code_domain_text) VALUES ('LANG', 'Languages of application');
-INSERT INTO ecm.domainValue(value, domain_id) values("ESP", (SELECT domain_id from ecm.domain where code_domain="LANG"));
-INSERT INTO ecm.domainValue(value, domain_id) values("CAT", (SELECT domain_id from ecm.domain where code_domain="LANG"));
-INSERT INTO ecm.domainValue(value, domain_id) values("ENG", (SELECT domain_id from ecm.domain where code_domain="LANG"));
+INSERT INTO ecm.domain_value(value, domain_id) values("ESP", (SELECT domain_id from ecm.domain where code_domain="LANG"));
+INSERT INTO ecm.domain_value(value, domain_id) values("CAT", (SELECT domain_id from ecm.domain where code_domain="LANG"));
+INSERT INTO ecm.domain_value(value, domain_id) values("ENG", (SELECT domain_id from ecm.domain where code_domain="LANG"));
 
 COMMIT;
