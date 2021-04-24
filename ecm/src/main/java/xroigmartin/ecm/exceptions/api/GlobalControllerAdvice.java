1db34fb09cd52377d8d191e1dbf20e1bf630908d
Xavier Roig <xroigmartin@gmail.com>
2021-04-24 20:51:12 +0200

    refactor: clean imports

--- ecm/src/main/java/xroigmartin/ecm/exceptions/api/GlobalControllerAdvice.java
+++ ecm/src/main/java/xroigmartin/ecm/exceptions/api/GlobalControllerAdvice.java
@@ -3,14 +3,9 @@ package xroigmartin.ecm.exceptions.api;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import xroigmartin.ecm.exceptions.api.domain.DomainNotFoundException;
-import xroigmartin.ecm.exceptions.api.domain.value.DomainValueNotFoundException;
-import xroigmartin.ecm.exceptions.domain.CodeDomainExistsException;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice extends ResponseEntityExceptionHandler{
