ee915c97ab51ff6c937d573d71bd532ec5905c01
Xavier Roig <xroigmartin@gmail.com>
2021-05-11 09:15:50 +0200

    Enable global CORS

--- ecm/src/main/java/xroigmartin/ecm/configuration/EcmConfiguration.java
+++ ecm/src/main/java/xroigmartin/ecm/configuration/EcmConfiguration.java
@@ -3,6 +3,8 @@ package xroigmartin.ecm.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class EcmConfiguration {
@@ -10,5 +12,17 @@ public class EcmConfiguration {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+			
+		};
 	}
 }
